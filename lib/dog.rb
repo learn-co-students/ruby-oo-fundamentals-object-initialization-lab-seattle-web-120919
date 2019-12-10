41664083e9a1152eb7919ae4e6a8b3625ad09e82
aldenweaver <aldenweavercodes@gmail.com>
2019-12-10 15:50:24 -0800

    Cleaned up code

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,5 @@ class Dog
   def initialize(name, breed="Mutt") 
     @name = name
     @breed = breed
-    # if !breed
-    #   @breed = "Mutt"
-    # else
-    #   @breed = breed
-    # end
   end
 end
