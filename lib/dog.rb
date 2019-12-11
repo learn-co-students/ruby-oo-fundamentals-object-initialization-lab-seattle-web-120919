fdcc5f966e076ebb9dbee530029feb70816a23b4
kkirby16 <kkirby16@my.whitworth.edu>
2019-12-11 12:28:14 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+    def initialize(name, breed= "Mutt")
+    @name = name
+    @breed = breed #assigns mutt to mutt. 
+    end  
+end 
+
+#when make a dog want to say dog.name then you can get the name. 
+    
+    
+    
+
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person 
+def initialize(name)
+@name = name 
+
+end  
+
+def name 
+    @name # this method is a reader 
+end 
+end 
+
+
+
+
+
+
