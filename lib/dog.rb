18cd8859a2c57cdbcd0e4d14d6a0d9f8cfccc915
kkirby16 <kkirby16@my.whitworth.edu>
2020-01-21 11:33:49 -0800

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,6 @@
 class Dog
-
-def initialize(name, breed= "Mutt")
-@name = name 
-@breed = breed
-
-end 
-end 
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+end
